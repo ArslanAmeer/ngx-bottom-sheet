d0fde1fd88684cad89b7808c7dec2897a225f427
Arslan Ameer <arslan.ameer@hotmail.com>
2024-09-05 15:45:51 +0500

    ♻️ Update: scripts

---
+++ scripts/post-build.js
@@ -0,0 +1,4 @@
+const fs = require('node:fs');
+
+fs.copyFileSync('README.md', 'dist/ngx-bottom-sheet/README.md');
+fs.copyFileSync('ngx-bottom-sheet.png', 'dist/ngx-bottom-sheet/ngx-bottom-sheet.png');
