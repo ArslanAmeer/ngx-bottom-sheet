6d60152210db273775ac3c624fd3a2838a3fe066
Arslan Ameer <arslan.ameer@hotmail.com>
2024-09-05 15:45:22 +0500

    ♻️ Update: demo

--- projects/ngx-bottom-sheet-demo/src/app/app.component.ts
+++ projects/ngx-bottom-sheet-demo/src/app/app.component.ts
@@ -1,23 +1,59 @@
-import { Component } from '@angular/core';
-import { NgxBottomSheetService } from 'ngx-bottom-sheet';
-import { SampleComponent } from './sample/sample.component';
+import {Component} from '@angular/core';
+import {NgxBottomSheetService} from 'ngx-bottom-sheet';
+import {SampleComponent} from './sample/sample.component';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngx-bottom-sheet-demo';
+  input = '';
 
-  constructor(private readonly _bottomSheetService: NgxBottomSheetService) {}
+  constructor(private readonly _bottomSheetService: NgxBottomSheetService) {
+  }
 
   openBottomSheet() {
     this._bottomSheetService.open(SampleComponent, {
-      height: '50%',
+      height: 'mid',
       closeOnBackdropClick: false,
     });
+  }
+
+
+  openBottomSheetToCLoseFromBackdropClick() {
+    this._bottomSheetService.open(SampleComponent, {
+      height: 'mid',
+      closeOnBackdropClick: true,
+    });
+  }
+
+  openBottomSheetWithCloseFromInsideComponent() {
+    this._bottomSheetService.open(SampleComponent, {
+      height: 'top',
+      closeOnBackdropClick: false,
+      data: {
+        closeFromInside: true
+      }
+    }).afterClosed$.subscribe((data: any) => {
+      console.log('Data from inside component: ', data);
+    });
+  }
+
+
+  openBottomSheetAndPassData() {
+    this._bottomSheetService.open(SampleComponent, {
+      height: '50%',
+      closeOnBackdropClick: false,
+      data: {
+        text: this.input
+      },
+    })
   }
 }
--- projects/ngx-bottom-sheet-demo/src/app/sample/sample.component.ts
+++ projects/ngx-bottom-sheet-demo/src/app/sample/sample.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {NgxBottomSheetService} from "ngx-bottom-sheet";
 
 @Component({
   selector: 'app-sample',
@@ -9,4 +10,29 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './sample.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SampleComponent {}
+export class SampleComponent implements OnInit {
+
+  bottomSheetData: any;
+
+  closeFromInside = false;
+  showPassedData = false;
+
+  constructor(private readonly _bottomSheetService: NgxBottomSheetService) {
+  }
+
+  ngOnInit() {
+    if (this.bottomSheetData) {
+      console.log('Data from auto populated bottomSheetData property by Bottom Sheet Service: ', this.bottomSheetData);
+
+      console.log('Data from injecting Bottom Service: ', this._bottomSheetService.currentSheetData);
+
+      this.closeFromInside = this.bottomSheetData.closeFromInside;
+      this.showPassedData = this.bottomSheetData.text;
+
+    }
+  }
+
+  closeDialog(data: any) {
+    this._bottomSheetService.close(data);
+  }
+}
