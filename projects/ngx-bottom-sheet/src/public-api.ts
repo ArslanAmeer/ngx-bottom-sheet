92d4d1474299d7ff50698c447bbe4a97a7c35e71
Arslan Ameer <arslan.ameer@hotmail.com>
2024-09-07 14:14:15 +0500

    ♻️ Update: Only expose bottom sheet service

--- projects/ngx-bottom-sheet/src/public-api.ts
+++ projects/ngx-bottom-sheet/src/public-api.ts
@@ -2,7 +2,5 @@
  * Public API Surface of ngx-bottom-sheet
  */
 
-export * from './lib/ngx-bottom-sheet.types';
 export * from './lib/ngx-bottom-sheet.service';
-export * from './lib/ngx-bottom-sheet.component';
 
